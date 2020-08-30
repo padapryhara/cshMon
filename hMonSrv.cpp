76d94644f92a1b7b344af3e99e2e9a83571275db
Anton Padapryhara <anton.padapryhara@logicblox.com>
2020-08-30 19:16:17 +0300

    pistache support was added

--- hMonSrv.cpp
+++ hMonSrv.cpp
@@ -1,13 +1,55 @@
+#include <mutex> 
 #include <string>
 #include <future>
 #include <iostream>
 #include <numeric>
 
 #include <zmqpp/zmqpp.hpp>
+#include <pistache/endpoint.h>
 #include "hMonitor.h"
 
 using namespace std;
 
+//web-server code
+using namespace Pistache;
+
+struct AtomicStatus
+{
+    string get() const
+    {
+      const std::lock_guard<std::mutex> guard(statusLock);
+      return string(status);
+    }
+
+    void set(const std::string& value)
+    {
+      const std::lock_guard<std::mutex> guard(statusLock);
+      status = value;
+    }
+
+  private:  
+    mutable std::mutex statusLock;
+    std::string status;
+};
+
+class SubSystemStatusHandler : public Http::Handler
+{
+  public:
+    explicit SubSystemStatusHandler(AtomicStatus& status)
+      : subSystemStatus(status)
+    { }
+
+    HTTP_PROTOTYPE(SubSystemStatusHandler)
+
+    void onRequest(const Http::Request& request, Http::ResponseWriter response) override
+    {
+      UNUSED(request);
+      response.send(Http::Code::Ok, subSystemStatus.get(), MIME(Application, Json));
+    }
+
+  private:
+    AtomicStatus& subSystemStatus;
+};
 
 //command processor
 static void waitForCmd(zmqpp::socket& socket)
@@ -22,7 +64,7 @@ static void waitForCmd(zmqpp::socket& socket)
       socket.send(message);
 
       string buffer;
-      socket.receive(buffer);
+      socket.receive(buffer); //TODO: ACK with error code
     }    	
   }
 }
@@ -34,8 +76,20 @@ int main(int argc, char *argv[])
   {
   	cout << "Usage: " << string(argv[0]) << " <client_ip_address>"  << endl;
   	return -1;
-  }	
+  }
 
+  //web-server init
+  Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(hMonitor::webServerPort));
+  auto opts = Http::Endpoint::options().threads(1);
+  Http::Endpoint server(addr);
+  
+  server.init(opts);
+  AtomicStatus status;
+  auto webHandler = Http::make_handler<SubSystemStatusHandler>(status);
+  server.setHandler(webHandler);
+  server.serveThreaded();
+
+  //endpoints
   const string hbEndpoint = "tcp://" + string(argv[1]) + ":" + to_string(hMonitor::heartbeatPort);
   const string cmdEndpoint = "tcp://" + string(argv[1]) + ":" + to_string(hMonitor::commandPort);
 
@@ -59,9 +113,12 @@ int main(int argc, char *argv[])
 
     string buffer;
     socketHb.receive(buffer);
+    status.set(buffer);
     
     cout << "Response: " << endl << buffer << endl;
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
   }
+
+  //TODO: server.shutdown();
 }
